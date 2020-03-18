2f553d7ec458e8cc7133ba10adcfc427bd5f4e99
Skylar Wong <skylar@Skylars-MacBook-Air.local>
2020-03-18 10:46:42 -0500

    connect models and register admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Superhero
 
-class Superhero:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, power, description, age):
-    self.name = name
-    self.power = power
-    self.description = description
-    self.age = age
-
-superheroes = [
-  Superhero('Wonder Woman', 'Amazon', 'princess of themyscira', 3),
-  Superhero('Iron Man', 'super smart', 'playboy millionaire philanthropist', 0),
-  Superhero('Captain Marvel', 'OP', 'inhaled inifinity stone power', 4)
-]
+# class Superhero:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, power, description, age):
+#     self.name = name
+#     self.power = power
+#     self.description = description
+#     self.age = age
 
 
 # Create your views here.
@@ -23,4 +18,5 @@ def about(request):
     return render(request, 'about.html')
 
 def superheroes_index(request):
+    superheroes = Superhero.objects.all()
     return render(request, 'superheroes/index.html', {'superheroes': superheroes})
